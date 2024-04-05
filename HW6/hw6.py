14109c188ddf3caf0fe9261b6905516fa4660f1a
Diegs18 <npdhockey@gmail.com>
2024-04-05 06:49:05 -0700

    hw6 is done turning it in. checklist completed

--- HW6/hw6.py
+++ HW6/hw6.py
@@ -12,28 +12,26 @@
 import numpy as np
 from random import random
 
-precisions = [0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001]
+precisions = [0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001] #list of precision of pi we want to check for
 for prec in precisions:
     success_cnt = 0
     sum_pi = 0
-    for j in range(1,101):
+    for j in range(1,101): #100 attempts of calculating pi
         pi_cnt = 0
         ave_pi = 0
-        for i in range(1, 10001):
+        for i in range(1, 10001): #10,000 samples of trying to calculate pi
             x = random()
             y = random()
             z = np.sqrt(x**2 + y**2)
-            if z<1:
+            if z<1: #do we count the value?
                 pi_cnt += 1
-            calc_pi = 4*pi_cnt/i
-            if abs(np.pi - calc_pi) < prec:
+            calc_pi = (4*pi_cnt) / i
+            if abs(np.pi - calc_pi) < prec: #is our cal pi close enough?
                 sum_pi += calc_pi
                 success_cnt += 1
-                break;
-    #ave_pi = sum_pi
-
+                break; #do next attampt
     if(success_cnt<1):
         print(prec, "no success")
     else:    
-        ave_pi = sum_pi/success_cnt
+        ave_pi = sum_pi / success_cnt
         print(prec, "success", success_cnt, "times", ave_pi)
